376a6ae1a96b0a743145d8ee0426770c729aa23d
Sam Tran <samuelttran@gmail.com>
2024-02-03 08:50:07 -0700

    Create methods #dnd_client and #query_monster_names in dndService, will need to update tests

--- app/services/dnd_service.rb
+++ app/services/dnd_service.rb
@@ -1,120 +1,21 @@
 class DndService
-  def conn
-    Faraday.new(url: "https://www.dnd5eapi.co/graphql")
+  def dnd_client
+    # NOTE we can also include another argument here for http_options:  read_timeout: # }
+    # To error if it takes too long for a response to get back from dnd graphql
+    Graphlient::Client.new(
+      'https://www.dnd5eapi.co/graphql',
+      headers: { 'Content-Type' => 'application/json' }
+    )
   end
-
-
-  def query_monsters
-    graphql_query = <<~GRAPHQL
-      {
+  
+  def query_monster_names
+    response = dnd_client.query <<~GRAPHQL
+      query {
         monsters {
           name
           index
         }
       }
     GRAPHQL
-
-    response = conn.post do |request|
-      request.headers['Content-Type'] = 'application/json'
-      request.body = { query: graphql_query }.to_json
-    end
-
-    data = JSON.parse(response.body, symbolize_names: true)
   end
-
-  # def query_monsters
-  #   query = <<~GRAPHQL
-  #     query Monsters {
-  #       monsters {
-  #         name
-  #         index
-  #       }
-  #     }
-  #   GRAPHQL
-
-  #   response = HTTParty.post(
-  #     "https://www.dnd5eapi.co/graphql",
-  #     headers:{ 'content-type' => 'application/json' },
-  #     body: query.to_json
-  #   )
-  #   data = JSON.parse(response.body, symbolize_names: :true)
-  #   # require 'pry'; binding.pry
-
-  # end
-
-  # class << self
-  #   attr_accessor :base_site, :base_headers
-
-  #   def base_headers
-  #     @base_headers ||= {
-  #         'Content-Type' => 'application/json',
-  #         'Accept' => 'application/json'
-  #     }
-  #   end
-  # end
-
-  # attr_accessor :site, :headers, :body, :response
-
-  # def initialize(**args)
-  #   @site = "https://www.dnd5eapi.co/graphql" || args[:site] || base_site
-  #   headers.merge!(args[:headers].to_h)
-  #   self
-  #       @my_query = <<~GRAPHQL
-  #     query Monsters {
-  #       monsters {
-  #         name
-  #         index
-  #       }
-  #     }
-  #   GRAPHQL
-  # end
-
-  # def query_monsters(**args)
-  #   self.body = {}
-  #   body["query"] = @my_query || raise(ArgumentError 'Missing query string.')
-  #   # body["variables"] = args[:variables]
-  #   # body["operationName"] = args[:operation_name]
-
-  #   self.response = post
-  #   require 'pry'; binding.pry
-  # end
-
-  # def headers
-  #   @headers ||= base_headers.to_h
-  # end
-
-  # private
-
-  # def base_site
-  #   self.class.base_site
-  # end
-
-  # def base_headers
-  #   self.class.base_headers
-  # end
-
-  # def faraday_module
-  #   Faraday
-  # end
-
-  # def json_parser
-  #   JSON
-  # end
-
-  # def new_faraday_connection
-  #   faraday_module.new
-  # end
-
-  # def body_json
-  #   json_parser.generate(body)
-  # end
-
-  # def post
-  #   new_faraday_connection.post do |req|
-  #     req.url site
-  #     req.headers.merge! headers
-  #     req.body = body_json
-  #   end
-  # end
-
 end
--- spec/services/dnd_service_spec.rb
+++ spec/services/dnd_service_spec.rb
@@ -1,46 +1,34 @@
 require 'rails_helper'
 
-# {
-#     "data": {
-#         "monsters": [
-#             {
-#                 "name": "Acolyte",
-#                 "index": "acolyte"
-#             },
-#             {
-#                 "name": "Aboleth",
-#                 "index": "aboleth"
-#             },
-
 describe DndService, type: :service do
   describe 'DndService GrapQL API service methods', :vcr do
-    xit '#conn, returns a Faraday object ' do
+    it '#dnd_client, returns a Faraday object ' do
       service = DndService.new
-      expect(service.conn).to be_a(Faraday::Connection)
+      expect(service.dnd_client).to be_a(Graphlient::Client)
     end
 
-    it '#query_monsters, returns all monsters, name and index only' do
-      service = DndService.new
-      query_response = service.query_monsters
-# require 'pry'; binding.pry
-      # expect(response).to be_successful
-      # expect(response.status).to eq(200)
-      expect(query_response).to be_a(Hash)
+    # it '#query_monsters, returns all monsters, name and index only' do
+    #   service = DndService.new
+    #   query_response = service.query_monsters
 
-      expect(query_response).to have_key(:data)
+    #   expect(query_response.status).to eq(200)
+    #   expect(query_response).to be_a(Hash)
 
-      data = query_response[:data]
-      expect(data).to have_key(:monsters)
+    #   expect(query_response).to have_key(:data)
 
-      monsters = data[:monsters]
-      expect(monsters).to be_a(Array)
+    #   data = query_response[:data]
+    #   expect(data).to have_key(:monsters)
 
-      monsters.each do |monster|
-        expect(monster).to have_key(:name)
-        expect(monster[:name]).to be_a(String)
-        expect(monster).to have_key(:index)
-        expect(monster[:index]).to be_a(String)
-      end
-    end
+    #   monsters = data[:monsters]
+    #   expect(monsters).to be_a(Array)
+
+    #   monsters.each do |monster|
+    #     expect(monster).to have_key(:name)
+    #     expect(monster[:name]).to be_a(String)
+    #     expect(monster).to have_key(:index)
+    #     expect(monster[:index]).to be_a(String)
+    #   end
+    # end
+
   end
 end
