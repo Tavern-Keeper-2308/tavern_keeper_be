040d57e6368f275206b6a817143ce3d11946ba44
Xander Hendry <vhendry.coding@gmail.com>
2024-02-07 15:54:56 -0500

    update seed data to fit FE request

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,43 +8,64 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-lone_user = { user_name: "IAmLonely"}
+# users
+demo_no_encounters = { user_name: "demo-no-encounters"}
 
-encounter1 = FactoryBot.create(:encounter)
-FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "aboleth")
+demo_one_encounter = { user_name: "demo-one-encounter"}
 
-encounter2 = FactoryBot.create(:encounter)
-FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "aboleth")
-FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "aboleth")
+demo_many_encounters = { user_name: "demo-many-encounters"}
 
-encounter3 = FactoryBot.create(:encounter)
-FactoryBot.create(:encounter_monster, encounter: encounter3, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounter3, monster_name: "aboleth")
+# encounter for user demo_one_encounter
+encounter = FactoryBot.create(:encounter, user_name: demo_one_encounter)
+FactoryBot.create(:encounter_monster, encounter: encounter, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter, monster_name: "Goblin")
 
-encounterG1 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
-encounterG2 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
-encounterG3 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
-encounterG4 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
-encounterG5 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
+# encounters for user demo_many_encounters
+encounter1 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "Giant Shark")
+FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "Aboleth")
+FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "Aboleth")
 
-FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG2, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG2, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG4, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG4, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
-FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
+encounter2 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "Goblin")
+
+encounter3 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter3, monster_name: "Aboleth")
+FactoryBot.create(:encounter_monster, encounter: encounter3, monster_name: "Aboleth")
+
+encounter4 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter4, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter4, monster_name: "Aboleth")
+
+encounter5 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter5, monster_name: "Kobold")
+FactoryBot.create(:encounter_monster, encounter: encounter5, monster_name: "Kobold")
+FactoryBot.create(:encounter_monster, encounter: encounter5, monster_name: "Ancient Red Dragon")
+
+encounter6 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter6, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter6, monster_name: "Goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter6, monster_name: "Frog")
+
+encounter7 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter7, monster_name: "Gelatinous Cube")
+FactoryBot.create(:encounter_monster, encounter: encounter7, monster_name: "Gelatinous Cube")
+
+encounter8 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter8, monster_name: "Giant Bat")
+FactoryBot.create(:encounter_monster, encounter: encounter8, monster_name: "Giant Bat")
+
+encounter9 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter9, monster_name: "Ghast")
+FactoryBot.create(:encounter_monster, encounter: encounter9, monster_name: "Ghast")
+
+encounter10 = FactoryBot.create(:encounter, user_name: demo_many_encounters)
+FactoryBot.create(:encounter_monster, encounter: encounter10, monster_name: "Ancient Red Dragon")
+FactoryBot.create(:encounter_monster, encounter: encounter10, monster_name: "Ancient Red Dragon")
+FactoryBot.create(:encounter_monster, encounter: encounter10, monster_name: "Ancient Red Dragon")
+
+
