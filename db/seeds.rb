89ebb1d44e8180d633a758832bfc63166435dd40
Sam Tran <samuelttran@gmail.com>
2024-02-07 12:06:14 -0700

    Add starter seed data for encounters

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,44 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+lone_user = { user_name: "IAmLonely"}
+
+encounter1 = FactoryBot.create(:encounter)
+FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter1, monster_name: "aboleth")
+
+encounter2 = FactoryBot.create(:encounter)
+FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "aboleth")
+FactoryBot.create(:encounter_monster, encounter: encounter2, monster_name: "aboleth")
+
+encounter3 = FactoryBot.create(:encounter)
+FactoryBot.create(:encounter_monster, encounter: encounter3, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounter3, monster_name: "aboleth")
+
+encounterG1 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
+encounterG2 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
+encounterG3 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
+encounterG4 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
+encounterG5 = FactoryBot.create(:encounter, user_name: "GoblinKiller")
+
+FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG1, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG2, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG2, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG3, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG4, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG4, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
+FactoryBot.create(:encounter_monster, encounter: encounterG5, monster_name: "goblin")
