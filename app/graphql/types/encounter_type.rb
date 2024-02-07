5ce8f69fc4ec610caadc1b2e335411f6c1c456f4
Kevin Zolman <kevin.zolman@gmail.com>
2024-02-07 16:35:10 -0700

    Add monsters to encounter type

--- app/graphql/types/encounter_type.rb
+++ app/graphql/types/encounter_type.rb
@@ -9,6 +9,7 @@ module Types
     field :summary, String
     field :description, String
     field :treasure, String
+    field :monsters, [Types::MonsterType]
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
   end
