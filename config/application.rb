aeb92b3cf762d4af5dd7ee8a33b2f508d61c3f33
Kevin Zolman <kevin.zolman@gmail.com>
2024-02-08 12:38:21 -0700

    Modify config/application

--- config/application.rb
+++ config/application.rb
@@ -30,8 +30,6 @@ module TavernKeeper
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
-    config.autoload_paths << Rails.root.join('lib')
-    config.autoload_paths << Rails.root.join('app', 'facades')
     config.autoload_lib(ignore: %w(assets tasks))
 
     # Configuration for the application, engines, and railties goes here.
