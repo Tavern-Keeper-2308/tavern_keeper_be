5cc9c157df59b3e9e9587ebb0a7844bf013d8245
Xander Hendry <vhendry.coding@gmail.com>
2024-02-08 19:55:04 -0500

    change encounter factory faker

--- spec/factories/encounters.rb
+++ spec/factories/encounters.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :encounter do
     user_name { "Drizzt" }
-    encounter_name { Faker::Books::Lovecraft.fhtagn }
+    encounter_name { Faker::JapaneseMedia::StudioGhibli.quote }
     party_size { rand(1..4) }
     party_level { rand(1..20) }
     summary { Faker::Fantasy::Tolkien.poem }
