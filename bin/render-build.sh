3a6edf76b3abcd57a9d23645dde0905fbb19eeb7
Xander Hendry <vhendry.coding@gmail.com>
2024-09-19 17:22:06 -0400

    add render.yml and build files

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,4 +3,11 @@
 set -o errexit
 
 bundle install
-./bin/rails db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+# bundle exec rails db:migrate
